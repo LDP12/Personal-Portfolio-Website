172599c0a85ab93194c4c1eb8b44ff5f7d42eb6c
LDP <lukedanpenney@gmail.com>
2024-12-30 18:13:15 +0200

    Change for Github Deployment (Production or Dev check)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Personal-Portfolio-Website',
-  plugins: [react()],
+  base: import.meta.env.MODE === 'production'
+      ? '/Personal-Portfolio-Website/'
+      : '/',
+  plugins: [react()]
 })
+
